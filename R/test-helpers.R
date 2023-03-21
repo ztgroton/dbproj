77ddc2ac61452026389d5d525e3dd5a8ce9e032e
ztgroton <ztgroton@gmail.com>
2023-03-21 09:58:20 -0400

    Fixed big in 'test-helpers.R'

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -2,16 +2,16 @@
 expect_s3_semantics <- function(object, n) {
   
   # Use R Quasi-Quotation to capture Actual Value (act$val) and Label (act$lab)
-  act <- quasi_label(rlang::enquo(object), arg = "object")
+  act <- testthat::quasi_label(rlang::enquo(object), arg = "object")
   
   
   act$n <- length(act$val)
   
   if (act$n == n) {
-    succeed()
+    testthat::succeed()
     return(invisible(act$val))
   }
   
   message <- sprintf("%s has length %i, not length %i.", act$lab, act$n, n)
-  fail(message)
+  testthat::fail(message)
 }
