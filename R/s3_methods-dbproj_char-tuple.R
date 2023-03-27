2f039a12b2a920247e0781054adca09024c5b94e
ztgroton <ztgroton@gmail.com>
2023-03-27 14:50:47 -0400

    Enhanced implementation of S3 Methods for class 'dbproj_char_tuple'.

--- R/s3_methods-dbproj_char-tuple.R
+++ R/s3_methods-dbproj_char-tuple.R
@@ -2,15 +2,40 @@
 #' S3 Method - Test Equality of S3 'dbproj_char_tuple' Instances
 #'
 #' @param obj S3 Object
+#' @param x S3 Object 
 #' @param ... R ellipsis
 #'
-#' @return S3 Object
+#' @return logical
 #'
 #' @examples
 #' \dontrun{
-#' test <- equals(s3_obj, FALSE)
+#' test <- equals(s3_obj, s3_obj)
 #' }
-equals.dbproj_char_tuple <- function(obj, ...) {}
+equals.dbproj_char_tuple <- function(obj, x, ...) {
+  
+  # Validate Inputs 
+  if (missing(obj)) {stop("`obj` is missing in call to `insert.dbproj_char_tuple`")}
+  if (missing(x)) {stop("`x` is missing in call to `insert.dbproj_char_tuple`")}
+  
+  # Validate Input Expectations
+  
+  # * `obj`
+  if (!isTRUE(inherits(obj, 'dbproj_char_tuple'))) {
+    stop("`obj` must inherit from 'dbproj_char_tuple' in call to `insert.dbproj_char_tuple`")
+  }
+  
+  # * `x`
+  if (!isTRUE(inherits(x, 'dbproj_char_tuple'))) {
+    stop("`x` must inherit from 'dbproj_char_tuple' in call to `insert.dbproj_char_tuple`")
+  }
+  
+  # Test for Equality
+  is_equal <- isTRUE(identical(sort(obj$data$tuple), sort(x$data$tuple)))
+  
+  # Return Equality
+  return(is_equal)
+  
+}
 
 #' S3 Method - CRUD 'Read' Operation
 #'
@@ -26,64 +51,163 @@ equals.dbproj_char_tuple <- function(obj, ...) {}
 contents.dbproj_char_tuple <- function(obj, ...) {
   
   # Validate Inputs 
-  if (missing(obj)) {stop("`obj` is missing in call to `contents.dbproj_char_tuple`")}
+  if (missing(obj)) {stop("`obj` is missing in call to `insert.dbproj_char_tuple`")}
   
-  # Prepare Contents if not Empty
-  obj_contents <- c(obj$name, obj$type)
+  # Validate Input Expectations
+  
+  # * `obj`
+  if (!isTRUE(inherits(obj, 'dbproj_char_tuple'))) {
+    stop("`obj` must inherit from 'dbproj_char_tuple' in call to `insert.dbproj_char_tuple`")
+  }
   
   # Return Contents
-  return(obj_contents)
+  return(obj$data$tuple)
   
 }
 
 #' S3 Method - CRUD 'Insert' Operation
 #'
 #' @param obj S3 Object
+#' @param x character
+#' @param in_place logical
 #' @param ... R ellipsis
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' insert(obj, ...)
+#' insert(obj, x)
+#' output <- insert(obj, x, in_place = FALSE)
 #' }
-insert.dbproj_char_tuple <- function(obj, ...) {}
+insert.dbproj_char_tuple <- function(obj, x, in_place = TRUE, ...) {
+  
+  # Validate Inputs 
+  if (missing(obj)) {stop("`obj` is missing in call to `insert.dbproj_char_tuple`")}
+  if (missing(in_place)) {in_place <- TRUE}
+  
+  # Validate Input Expectations
+  
+  # * `obj`
+  if (!isTRUE(inherits(obj, 'dbproj_char_tuple'))) {
+    stop("`obj` must inherit from 'dbproj_char_tuple' in call to `insert.dbproj_char_tuple`")
+  }
+  
+  # * `x`
+  if (!isTRUE(validate.character(obj = x, allow_empty = FALSE, allow_na = FALSE))) {
+    stop("`x` must be non-empty character vector with no NA or NULL values in call to `insert.dbproj_char_tuple`")
+  }
+  
+  # * `in_place`
+  if (!isTRUE(validate.logical(obj = in_place, single = TRUE))) {
+    stop("`in_place` must equal TRUE/FALSE in call to `insert.dbproj_char_tuple`")
+  }
+  
+  # Insert Contents of `x` into `obj$data$tuple`
+  obj$data$tuple <- unique(union(obj$data$tuple, x))
+  
+  # Conditionally Return Input `obj` (Standard R Style)
+  if (isTRUE(in_place)) {
+    invisible(TRUE)
+  } else {
+    invisible(obj)
+  }
+  
+}
 
 #' S3 Method - CRUD 'Update' Operation
 #'
 #' @param obj S3 Object
+#' @param x character
+#' @param in_place logical
 #' @param ... R ellipsis
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' update(obj, ...)
+#' update(obj, x)
+#' output <- update(obj, x, in_place = FALSE)
 #' }
-update.dbproj_char_tuple <- function(obj, ...) {}
+update.dbproj_char_tuple <- function(obj, x, in_place = FALSE, ...) {
+  
+  # Validate Inputs 
+  if (missing(obj)) {stop("`obj` is missing in call to `update.dbproj_char_tuple`")}
+  if (missing(in_place)) {in_place <- TRUE}
+  
+  # Validate Input Expectations
+  
+  # * `obj`
+  if (!isTRUE(inherits(obj, 'dbproj_char_tuple'))) {
+    stop("`obj` must inherit from 'dbproj_char_tuple' in call to `update.dbproj_char_tuple`")
+  }
+  
+  # * `x`
+  if (!isTRUE(validate.character(obj = x, allow_empty = FALSE, allow_na = FALSE))) {
+    stop("`x` must be non-empty character vector with no NA or NULL values in call to `update.dbproj_char_tuple`")
+  }
+  
+  # * `in_place`
+  if (!isTRUE(validate.logical(obj = in_place, single = TRUE))) {
+    stop("`in_place` must equal TRUE/FALSE in call to `update.dbproj_char_tuple`")
+  }
+  
+  # Update Contents of `obj$data$tuple`
+  obj$data$tuple <- unique(x)
+  
+  # Conditionally Return Input `obj` (Standard R Style)
+  if (isTRUE(in_place)) {
+    invisible(TRUE)
+  } else {
+    invisible(obj)
+  }
+  
+}
 
 #' S3 Method - CRUD 'Delete' Operation
 #'
 #' @param obj S3 Object
+#' @param x character
+#' @param in_place logical
 #' @param ... R ellipsis
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' delete(obj, ...)
+#' delete(obj, x)
+#' output <- delete(obj, x, in_place = FALSE)
 #' }
-delete.dbproj_char_tuple <- function(obj, ...) {}
-
-#' S3 Method - Alter Internal State of S3 Object Instances
-#'
-#' @param obj S3 Object
-#' @param ... R ellipsis
-#'
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' alter(obj, ...)
-#' }
-alter.dbproj_char_tuple <- function(obj, ...) {}
+delete.dbproj_char_tuple <- function(obj, x, in_place = FALSE, ...) {
+  
+  # Validate Inputs 
+  if (missing(obj)) {stop("`obj` is missing in call to `delete.dbproj_char_tuple`")}
+  if (missing(in_place)) {in_place <- TRUE}
+  
+  # Validate Input Expectations
+  
+  # * `obj`
+  if (!isTRUE(inherits(obj, 'dbproj_char_tuple'))) {
+    stop("`obj` must inherit from 'dbproj_char_tuple' in call to `delete.dbproj_char_tuple`")
+  }
+  
+  # * `x`
+  if (!isTRUE(validate.character(obj = x, allow_empty = FALSE, allow_na = FALSE))) {
+    stop("`x` must be non-empty character vector with no NA or NULL values in call to `delete.dbproj_char_tuple`")
+  }
+  
+  # * `in_place`
+  if (!isTRUE(validate.logical(obj = in_place, single = TRUE))) {
+    stop("`in_place` must equal TRUE/FALSE in call to `delete.dbproj_char_tuple`")
+  }
+  
+  # Update Contents of `obj$data$tuple`
+  obj$data$tuple <- unique(setdiff(obj$data$tuple, x))
+  
+  # Conditionally Return Input `obj` (Standard R Style)
+  if (isTRUE(in_place)) {
+    invisible(TRUE)
+  } else {
+    invisible(obj)
+  }
+  
+}
