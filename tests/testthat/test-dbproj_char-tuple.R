de1947eec66a8980b8fbd3ac73743bb0db5ae35e
ztgroton <ztgroton@gmail.com>
2023-03-27 14:51:47 -0400

    Created test suite for S3 class 'dbproj_char_tuple'.

--- tests/testthat/test-dbproj_char-tuple.R
+++ tests/testthat/test-dbproj_char-tuple.R
@@ -1,3 +1,52 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("`equals.dbproj_char_tuple` works", {
+  expect_equal(equals.dbproj_char_tuple(char_tuple(c('a')), char_tuple('a')), TRUE)
+  expect_equal(equals.dbproj_char_tuple(char_tuple(c('b')), char_tuple('b')), TRUE)
+  expect_equal(equals.dbproj_char_tuple(char_tuple(c('a')), char_tuple('c')), FALSE)
+  expect_equal(equals.dbproj_char_tuple(char_tuple(c('b')), char_tuple('c')), FALSE)
+})
+
+test_that("`contents.dbproj_char_tuple` works", {
+  expect_equal(contents.dbproj_char_tuple(char_tuple(c('a', 'b', 'c'))), c('a', 'b', 'c'))
+})
+
+test_that("`insert.dbproj_char_tuple` works", {
+  expect_equal(
+    equals.dbproj_char_tuple(
+      char_tuple(x = c('a', 'b')), 
+      insert.dbproj_char_tuple(
+        obj = char_tuple('a'), 
+        x = 'b', 
+        in_place = FALSE
+      )
+    ), 
+    TRUE
+  )
+}) 
+
+test_that("`update.dbproj_char_tuple` works", {
+  expect_equal(
+    equals.dbproj_char_tuple(
+      char_tuple(x = c('a', 'b')), 
+      update.dbproj_char_tuple(
+        obj = char_tuple(x = c('c', 'd')), 
+        x = c('a', 'b'), 
+        in_place = FALSE
+      )
+    ), 
+    TRUE
+  )
+})
+
+test_that("`delete.dbproj_char_tuple` works", {
+  expect_equal(
+    equals.dbproj_char_tuple(
+      char_tuple(x = c('a', 'c')), 
+      delete.dbproj_char_tuple(
+        obj = char_tuple(x = c('a', 'b', 'c', 'd')), 
+        x = c('b', 'd'), 
+        in_place = FALSE
+      )
+    ), 
+    TRUE
+  )
 })
